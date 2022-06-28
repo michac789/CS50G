c59a071ed8d6fe1cc12a7881de4354d4731caaad
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2022-06-28 12:48:57 +0800

    minor changes on source code 2 - breakout

--- source_codes/src02-breakout/breakout3/src/LevelMaker.lua
+++ source_codes/src02-breakout/breakout3/src/LevelMaker.lua
@@ -12,7 +12,7 @@
     in the game.
 ]]
 
-LevelMaker = Class{} 
+LevelMaker = Class{}
 
 --[[
     Creates a table of Bricks to be returned to the main game, with different
@@ -37,14 +37,14 @@ function LevelMaker.createMap(level)
                 * 32                    -- multiply by 32, the brick width
                 + 8                     -- the screen should have 8 pixels of padding; we can fit 13 cols + 16 pixels total
                 + (13 - numCols) * 16,  -- left-side padding for when there are fewer than 13 columns
-                
+
                 -- y-coordinate
                 y * 16                  -- just use y * 16, since we need top padding anyway
-            ) 
+            )
 
             table.insert(bricks, b)
         end
-    end 
+    end
 
     return bricks
 end
