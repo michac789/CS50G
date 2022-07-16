5935197c5c6af5ce0e623dded4fac68cdc562056
Michael Andrew Chan <67429929+michac789@users.noreply.github.com>
2022-07-16 13:41:28 +0800

    pset8 helicopter: make gem spawn rarer than coins, spawn at lower half of screen(harder to get)

--- 08-helicopter/Assets/Resources/Scripts/GemSpawner.cs
+++ 08-helicopter/Assets/Resources/Scripts/GemSpawner.cs
@@ -21,15 +21,15 @@ public class GemSpawner : MonoBehaviour {
 		while (true) {
 
 			// number of coins we could spawn vertically
-			int gemsThisRow = Random.Range(1, 4);
+			int gemsThisRow = 1;
 
 			// instantiate all coins in this row separated by some random amount of space
 			for (int i = 0; i < gemsThisRow; i++) {
-				Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+				Instantiate(prefabs[Random.Range(0, prefabs.Length)], new Vector3(26, Random.Range(-9, 1), 10), Quaternion.identity);
 			}
 
 			// pause 1-5 seconds until the next coin spawns
-			yield return new WaitForSeconds(Random.Range(1, 5));
+			yield return new WaitForSeconds(Random.Range(5, 10));
 		}
 	}
 }
